acf7c8cebb009024a1d38de9f17e4eefbde6b233
Vincenzo <v.nunziata16@gmail.com>
2021-03-10 22:43:19 +0100

    Angular 11 Migration

--- src/app/tables/row-dialog/row-dialog.component.spec.ts
+++ src/app/tables/row-dialog/row-dialog.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { RowDialogComponent } from './row-dialog.component';
 
@@ -6,7 +6,7 @@ describe('RowDialogComponent', () => {
   let component: RowDialogComponent;
   let fixture: ComponentFixture<RowDialogComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ RowDialogComponent ]
     })
