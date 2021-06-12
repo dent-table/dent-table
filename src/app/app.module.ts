8cdd9367da521c6290e683b3dd21b9f89df564d3
Vincenzo <v.nunziata16@gmail.com>
2021-06-13 00:06:51 +0200

    Removed (maybe?) unused imports from app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,3 @@
-import 'reflect-metadata';
-import '../polyfills';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
